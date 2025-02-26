60b714d1a149eb6be4ec8dd1c80646c7e5a2caa4
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2025-02-26 15:45:07 +0530

    push

---
+++ GFG/Matrix/Spirally traversing a matrix.cpp
@@ -0,0 +1,77 @@
+///////////Spirally traversing a matrix
+
+///////////https://www.geeksforgeeks.org/batch/gfg-160-problems/track/matrix-gfg-160/problem/spirally-traversing-a-matrix-1587115621
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int left = 0 , right = m-1;
+        int top =0 , bottom = n-1;
+        vector<int> ans;
+        while(top <= bottom && left <= right){
+            /// right
+            for(int i= left; i<=right; i++){
+                ans.push_back(mat[top][i]);
+            }
+            top++;
+            for(int i = top; i<=bottom; i++){
+                ans.push_back(mat[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    ans.push_back(mat[bottom][i]);
+                }
+            bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(mat[i][left]);
+                }
+            
+            left++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
