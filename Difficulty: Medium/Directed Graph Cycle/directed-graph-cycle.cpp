64348734b09ee8e1822254c85c9673b16b365ca1
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-31 20:38:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,39 +1,135 @@
+/////////using DFS 
+
+// class Solution {
+//   public:
+//     bool iscycleDFS(vector<vector<int>>& adj, int u ,vector<bool>& visited ,vector<bool>& inrecursion){
+//         visited[u] = true;
+//         inrecursion[u] = true;
+        
+//         for(auto &v : adj[u]){
+//             ////if not visited than we check for cycle in DFS
+            
+//             // if(visited[v] == false && iscycleDFS(adj,v,visited,inrecursion)){
+//             //     return true;
+//             // }
+//             ///////or
+//             if(visited[v] == false ) {
+//                 if(iscycleDFS(adj,v,visited,inrecursion)){
+//                     return true;
+//                 }
+//             }
+//             else if(inrecursion[v] == true){
+//                 return true;
+//             }
+//         }
+//         inrecursion[u] = false;
+//         return false;
+        
+//     }
+//     bool isCyclic(int V, vector<vector<int>> &edges) {
+//         // code here
+//         vector<vector<int>> adj(V);
+//         for(auto &e : edges){
+//             int u = e[0], v = e[1];
+//             adj[u].push_back(v);
+            
+//         }
+        
+//         vector<bool>visited(V,false);
+//         vector<bool>inrecursion(V,false);
+        
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false && iscycleDFS(adj,i,visited,inrecursion)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+////t-o(V+E)
+////s-o(V+E)
+
+/////////////Using BFS
+
+////in this we write khans algorithm to find topological sort 
+///as topological sort only works in directed acylic graph 
+///so if we can find topogical sort -- than there is no cycle other wise there is cycle
+
 class Solution {
   public:
-    bool iscycleDFS(vector<vector<int>>& adj, int u ,vector<bool>& visited ,vector<bool>& inrecursion){
-        visited[u] = true;
-        inrecursion[u] = true;
-        
-        for(auto &v : adj[u]){
-            ////if not visited than we check for cycle in DFS
-            if(visited[v] == false && iscycleDFS(adj,v,visited,inrecursion)){
-                return true;
-            }
-            else if(inrecursion[v] == true){
-                return true;
-            }
-        }
-        inrecursion[u] = false;
-        return false;
-        
-    }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>> adj(V);
         for(auto &e : edges){
             int u = e[0], v = e[1];
             adj[u].push_back(v);
-            
         }
         
-        vector<bool>visited(V,false);
-        vector<bool>inrecursion(V,false);
-        
-        for(int i=0;i<V;i++){
-            if(visited[i] == false && iscycleDFS(adj,i,visited,inrecursion)){
-                return true;
+        /////1 make indegree vector
+        vector<int>indegree(V,0);
+        for(int u=0;u<V;u++){
+            for(auto &v : adj[u]){
+                indegree[v]++;
             }
         }
-        return false;
+        ////2 fill vertex whose indree = 0 in  queue
+        queue<int>que;
+        for(int u=0;u<V;u++){
+            if(indegree[u] == 0){
+                que.push(u);
+            }
+        }
+        ///now travese in queue and perform the operations
+        int cnt=0;
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            cnt++;
+            for(auto &v : adj[u]){
+                indegree[v]--;
+                
+                if(indegree[v] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        if(cnt == V) return false; /// as we can find topological sort 
+        
+        return true; ///we can't find topological sort
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
