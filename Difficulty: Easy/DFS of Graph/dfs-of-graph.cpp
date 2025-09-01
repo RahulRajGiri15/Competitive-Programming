addf513f64d6df13e6e4602134a14aafd8efa9eb
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-01 17:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,35 +1,6 @@
-// class Solution {
-//   public:
-//     void DFS(unordered_map<int,vector<int>>& adj,int u,vector<int>&result,vector<bool>& visited){
-//         if(visited[u] == true) return ;
-//         visited[u] =  true;
-        
-//         result.push_back(u);
-//         for(int &v : adj[u]){
-//             if(!visited[v]){
-//                 DFS(adj,v,result,visited);
-//             }
-//         }
-//     }
-//     vector<int> dfs(vector<vector<int>>& mp) {
-//         int V = mp.size();
-//         /////making our adjency list 
-//         unordered_map<int,vector<int>> adj;
-//         for(int u=0;u<V;u++){ /// for row 
-//             for(int v=0; v<V; v++){ ///for column
-//                 if(mp[u][v] == 1){
-//                     adj[u].push_back(v);
-//                 }
-//             }
-//         }
-//         vector<int>result;
-//         vector<bool>visited(V,false);
-//         DFS(adj,0,result,visited);
-//         return result;
-//     }
-// };
 
 ////////////
+
 // class Solution {
 //   public:
 //     void DFS(vector<vector<int>>& adj,int u,vector<int>&result,vector<bool>& visited){
@@ -63,35 +34,100 @@
 
 //////////////
 
+// class Solution {
+//   public:
+//     void DFS(unordered_map<int,vector<int>>& adj,int u,vector<int>&result,vector<bool>& visited){
+//         if(visited[u] == true) return ;
+//         visited[u] =  true;
+        
+//         result.push_back(u);
+//         for(int &v : adj[u]){
+//             if(!visited[v]){
+//                 DFS(adj,v,result,visited);
+//             }
+//         }
+//     }
+//     vector<int> dfs(vector<vector<int>>& mp) {
+//         int V = mp.size();
+//         ///making our adjency list 
+//         unordered_map<int,vector<int>> adj;
+//         for(int u=0;u<V;u++){ /// for row 
+//             for(int v : mp[u]){
+//                 adj[u].push_back(v);
+//             }
+//         }
+//         vector<int>result;
+//         vector<bool>visited(V,false);
+//         DFS(adj,0,result,visited);
+//         return result;
+//     }
+// };
+
+///////////////
+
+
+
 class Solution {
   public:
-    void DFS(unordered_map<int,vector<int>>& adj,int u,vector<int>&result,vector<bool>& visited){
+    void DFSS(unordered_map<int,vector<int>>&adj,int u,vector<int>& result,vector<bool>& visited){
         if(visited[u] == true) return ;
-        visited[u] =  true;
         
+        visited[u] = true;
         result.push_back(u);
         for(int &v : adj[u]){
-            if(!visited[v]){
-                DFS(adj,v,result,visited);
+            if(visited[v] == false)
+                DFSS(adj,v,result,visited);
             }
         }
-    }
-    vector<int> dfs(vector<vector<int>>& mp) {
+        
+    
+    vector<int>dfs(vector<vector<int>>& mp) {
         int V = mp.size();
-        ///making our adjency list 
+        /////making our adjency list
         unordered_map<int,vector<int>> adj;
-        for(int u=0;u<V;u++){ /// for row 
+        for(int u=0;u<V;u++){ //// for row
             for(int v : mp[u]){
                 adj[u].push_back(v);
             }
         }
         vector<int>result;
         vector<bool>visited(V,false);
-        DFS(adj,0,result,visited);
+        DFSS(adj,0,result,visited);
         return result;
     }
 };
-///////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////
 
 // class Solution {
 //   public:
