cd928f95426c76d452e3d3234b0ee60a10325790
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-08 02:12:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,77 +1,30 @@
-// ////////BFS 
-
-// class Solution {
-//   public:
-//     // Function to return Breadth First Traversal of given graph.
-//     void BFS(unordered_map<int,vector<int>>& adj,int u,vector<bool>& visited,vector<int>& result){
-//         /////////for BFS  take a queue
-//         if(visited[u] == true) return ;
-//         visited[u] = true;
-//         queue<int>que;
-//         que.push(u);
-//         result.push_back(u);
-//         while(!que.empty()){
-//             int u = que.front();
-//             que.pop();
-//             for(auto &v : adj[u]){
-//                 if(visited[v] == false){
-//                     que.push(v);
-//                     visited[v] = true;
-//                     result.push_back(v);
-//                 }
-//             }
-//         }
-//     }
-//     vector<int> bfs(vector<vector<int>> &mp) {
-//         unordered_map<int ,vector<int>>adj;
-//         int V = mp.size();
-//         for(int u=0;u<V;u++){
-//             for(int v : mp[u]){
-//                 adj[u].push_back(v);
-//             }
-//         }
-//         vector<int>result;
-//         vector<bool>visited(V,false);
-//         BFS(adj,0,visited,result);
-//         return result;
-//     }
-// };
-
-/////////////////////////////////BFS-- in this we use queue 
-
-
 class Solution {
   public:
-    void BFSS(unordered_map<int,vector<int>>& adj,int u,vector<int>& result,vector<int> & visited){
-        queue<int>qu;
-        qu.push(u);
+    // Function to return Breadth First Traversal of given graph.
+    void bfss(vector<vector<int>>&adj,int u,vector<bool>&visited,vector<int>& res){
+        queue<int>que;
         visited[u] = true;
-        result.push_back(u);
-        while(!qu.empty()){
-            u = qu.front();
-            qu.pop();
+        que.push(u);
+         res.push_back(u);
+        while(!que.empty()){
+            int u = que.front();
+           
+            que.pop();
             for(auto &v : adj[u]){
-                if(!visited[v]){
-                    qu.push(v);
-                    result.push_back(v);
+                if(visited[v] == false){
+                    que.push(v);
                     visited[v] = true;
+                    res.push_back(v);
                 }
             }
         }
     }
-    vector<int> bfs(vector<vector<int>> &mp) {
-        int V = mp.size();
-        unordered_map<int,vector<int>>adj;
-        
-        for(int u=0;u<V;u++){
-            for(int &v : mp[u]){
-                adj[u].push_back(v);
-            }
-        }
-        vector<int>visited(V,false);
-        vector<int>result;
-        BFSS(adj,0,result,visited);
-        return result;
-        
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        int v = adj.size();
+        vector<bool>visited(v,false);
+        vector<int> res;
+        bfss(adj,0,visited,res);
+        return res;
     }
 };
