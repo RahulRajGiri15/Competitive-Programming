a76ef27b799412515efdd05b5c296f526ca408f7
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-08 11:08:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,120 +1,37 @@
-////////Checking if Graph is cyclic//////////
-
-//////////Using DFS
-
-// class Solution {
-//   public:
-//     bool iscycleDFS(vector<vector<int>>& adj,int u,vector<bool>& visited,int parent){
-//         visited[u] = true;
-//         for(auto &v : adj[u]){
-//             if(v == parent) continue;
-//             if(visited[v]){
-//                 return true; //// there is cycle present
-//             }
-//             if(iscycleDFS(adj,v,visited,u)){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-//     bool isCycle(int V, vector<vector<int>>& edges) {
-//         // convert all the edges to the adjaency list 
-//         vector<vector<int>>adj(V);
-//         for(auto &e : edges){
-//             int u = e[0],v=e[1];
-//             adj[u].push_back(v);
-//             adj[v].push_back(u); /// because it's a undirected graph
-//         }
-        
-//         vector<bool>visited(V,false);
-//         for(int i=0;i<V;i++){
-//             if(visited[i] == false && iscycleDFS(adj,i,visited,-1)){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-/////////////Using BFS
-
-// class Solution {
-//   public:
-//     bool iscycleBFS(vector<vector<int>>& adj,int u,vector<bool>& visited,int parent){
-//       queue<pair<int,int>>que;
-//       que.push({u,-1});
-//         visited[u] = true;
-//         while(!que.empty()){
-//             pair<int,int> p = que.front();
-//             que.pop();
-//             int source = p.first;
-//             int parent = p.second;
-            
-//             for(int &v : adj[source]){
-//                 if(visited[v] == false){
-//                     visited[v] = true;
-//                     que.push({v,source});
-//                 }
-//                 else if(v != parent){
-//                     return true;
-//                 }
-//             }
-//         }
-//             return false;
-//     }
-        
-//     bool isCycle(int V, vector<vector<int>>& edges) {
-//         // convert all the edges to the adjaency list 
-        
-//         vector<vector<int>>adj(V);
-//         for(auto &e : edges){
-//             int u = e[0],v=e[1];
-//             adj[u].push_back(v);
-//             adj[v].push_back(u); /// because it's a undirected graph
-//         }
-        
-//         vector<bool>visited(V,false);
-//         for(int i=0;i<V;i++){
-//             if(visited[i] == false && iscycleBFS(adj,i,visited,-1)){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-//////////////////////////////
-
-
 class Solution {
   public:
-    bool iscycleDFS(vector<vector<int>>& adj,int u,vector<bool>& visited,int parent){
+  
+    bool dfss(vector<vector<int>>&adj,int u,vector<bool>&visited,int parent){
         visited[u] = true;
-        
-        for(int &v : adj[u]){
+        for(auto &v : adj[u]){
             if(v == parent) continue;
-            if(visited[v] == true) return true; /// cycle is present
-            if(iscycleDFS(adj,v,visited,u) == true){
+            if(visited[v] == true)return true;
+            if(dfss(adj,v,visited,u)){
                 return true;
             }
         }
         return false;
     }
     bool isCycle(int V, vector<vector<int>>& edges) {
-        // it is undirected graph
-        // convert all the edges to the adjaency list 
-
+        // Code here
         vector<vector<int>>adj(V);
-        for(auto &e : edges){
-            int u = e[0], v= e[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u); 
-        }
-        vector<bool>visited(V,false);
-        for(int i=0;i<V;i++){
-            if(visited[i] == false && iscycleDFS(adj,i,visited,-1)){
-                return true;
+            for(auto &it : edges){
+                int u = it[0];
+                int v = it[1];
+                adj[u].push_back(v);
+                adj[v].push_back(u);
             }
+            
+        vector<bool>visited(V,false);
+        bool iscy = false;
+        for(int i=0;i<V;i++){
+            if(visited[i] == false && dfss(adj,i,visited,-1)){
+                iscy = true;
+                break;
+            }
+        }
+        if(iscy == true){
+            return true;
         }
         return false;
     }
