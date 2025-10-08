230b09dfd392e547f31b5f5864fa799c54269338
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-08 11:28:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,13 +1,65 @@
+// class Solution {
+//   public:
+  
+//     bool dfss(vector<vector<int>>&adj,int u,vector<bool>&visited,int parent){
+//         visited[u] = true;
+//         for(auto &v : adj[u]){
+//             if(v == parent) continue;
+//             if(visited[v] == true)return true;
+//             if(dfss(adj,v,visited,u)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     bool isCycle(int V, vector<vector<int>>& edges) {
+//         // Code here
+//         vector<vector<int>>adj(V);
+//             for(auto &it : edges){
+//                 int u = it[0];
+//                 int v = it[1];
+//                 adj[u].push_back(v);
+//                 adj[v].push_back(u);
+//             }
+            
+//         vector<bool>visited(V,false);
+//         bool iscy = false;
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false && dfss(adj,i,visited,-1)){
+//                 iscy = true;
+//                 break;
+//             }
+//         }
+//         if(iscy == true){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+////////////////////////////////
+
 class Solution {
   public:
   
     bool dfss(vector<vector<int>>&adj,int u,vector<bool>&visited,int parent){
+        queue<pair<int,int>>que;
+        que.push({u,parent});
         visited[u] = true;
-        for(auto &v : adj[u]){
-            if(v == parent) continue;
-            if(visited[v] == true)return true;
-            if(dfss(adj,v,visited,u)){
-                return true;
+        while(!que.empty()){
+            pair<int,int> p = que.front();
+            que.pop();
+            int source = p.first;
+            int parent = p.second;
+            for(auto &v : adj[source]){
+                if(visited[v] == false){
+                    visited[v] = true;
+                    que.push({v,source});
+                }
+                else if(v != parent){
+                    return true;
+                }
             }
         }
         return false;
