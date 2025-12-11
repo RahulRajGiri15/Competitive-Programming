10e6490f815f6aa1ce21f58d35cfc21bd4f9fddc
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-11 21:48:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,26 +1,55 @@
+// class Solution {
+//   public:
+//     void insertstack(stack<int> &st, int x){
+//         ///if empty or top elemnt is less x
+//         if(st.empty() || st.top() <= x){
+//             st.push(x);
+//             return;
+            
+//         }
+//         //otherwise
+//         int temp = st.top();
+//         st.pop();
+//         insertstack(st,x);
+//         st.push(temp);
+        
+//     }
+//     void sortStack(stack<int> &st) {
+//         // code here
+//         if(st.empty()) return;
+//         int x = st.top();
+//         st.pop();
+        
+//         sortStack(st);
+//         insertstack(st,x);
+//     }
+// };
+
+
+/////////////////
+
 class Solution {
   public:
-    void insertstack(stack<int> &st, int x){
-        ///if empty or top elemnt is less x
+    void insertintostack(stack<int>&st,int x){
         if(st.empty() || st.top() <= x){
             st.push(x);
             return;
-            
         }
-        //otherwise
-        int temp = st.top();
-        st.pop();
-        insertstack(st,x);
-        st.push(temp);
         
+        int firstel = st.top();
+        st.pop();
+        insertintostack(st,x);
+        st.push(firstel);
     }
     void sortStack(stack<int> &st) {
-        // code here
-        if(st.empty()) return;
-        int x = st.top();
+        if(st.empty()){
+            return;
+        }
+        int topp = st.top();
         st.pop();
+        sortStack(st); /// trust- lof
         
-        sortStack(st);
-        insertstack(st,x);
+        /// now insert topp at the correct positon
+        insertintostack(st,topp);
     }
 };
